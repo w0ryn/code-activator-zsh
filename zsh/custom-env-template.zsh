a664016232e34f6f9a25fd7b3b471021a432d929
Wryn Wagner <wagner.wryn@gmail.com>
2021-09-16 22:46:29 -0600

    wrong env var

--- zsh/custom-env-template.zsh
+++ zsh/custom-env-template.zsh
@@ -1,10 +1,10 @@
 #####################################################################
 
 # Store initial PATH; allows for safe PATH manipulation for a particular project
-export RESTORE_PATH="$PATH"
+export _CA__RESTORE_PATH="$PATH"
 
 # Store a list of project-specific environment variables; unset on env exit
-export RESTORE_ENV=(
+export _CA__RESTORE_ENV=(
 	__CUSTOM_ENV_ACTIVE
 	)
 
